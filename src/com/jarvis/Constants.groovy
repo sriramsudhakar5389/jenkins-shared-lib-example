f02be371d0d3ace6c2ef9c1d0869c3b370f20e14
sriramsudhakar <sriram.nit.jsr@gmail.com>
2020-04-29 22:51:05 -0400

    Initail updates

---
+++ src/com/jarvis/Constants.groovy
@@ -0,0 +1,7 @@
+package com.jarvis
+
+class Constants {
+    static final String SLACK_MESSAGE = "Sending Slack Notification"
+    static final String EMAIL_MESSAGE = "Sending Email"
+}
+
---
+++ vars/notify.groovy
@@ -0,0 +1,11 @@
+import com.jarvis.Constants
+
+def call(Map config=[:]) {
+    if (config.type == "slack") {
+        echo Constants.SLACK_MESSAGE
+        echo config.message
+    } else {
+        echo Constants.EMAIL_MESSAGE
+        echo config.message
+    }
+}

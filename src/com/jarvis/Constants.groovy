ae3f447f37ba37f1f871b26277a61e4baf79f009
sriramsudhakar <sriram.nit.jsr@gmail.com>
2020-04-30 10:06:43 -0400

    Updates to Constants

--- src/com/jarvis/Constants.groovy
+++ src/com/jarvis/Constants.groovy
@@ -1,7 +1,7 @@
 package com.jarvis
 
 class Constants {
-    static final String SLACK_MESSAGE = "Sending Slack Notification"
-    static final String EMAIL_MESSAGE = "Sending Email"
+    static final String SLACK_MESSAGE = "Sending Slack develop Notification"
+    static final String EMAIL_MESSAGE = "Sending Email develop "
 }
 
